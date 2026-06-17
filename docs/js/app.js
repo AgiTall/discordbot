dae806dfb84e7a301bb957b9cdb250719570c890
David <danilovdavid154@gmail.com>
2026-06-17 21:42:07 +0500

    0.5.6+

--- docs/js/app.js
+++ docs/js/app.js
@@ -7,7 +7,7 @@
 // КОНФИГУРАЦИЯ
 // =============================================
 const CONFIG = {
-  botVersion: 'v0.5.5',
+  botVersion: 'v0.5.6',
   inviteUrl:  'https://discord.com/oauth2/authorize?client_id=1513810717495525377&scope=bot%20applications.commands&permissions=8',
   supportUrl: 'https://discord.gg/YOUR_INVITE_CODE',
   storageKey: 'membot_settings',
